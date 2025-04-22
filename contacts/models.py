c4dce895b6e1d86948fa605909afa3decd73d619
naeemullah <naeem.ullah@veroke.com>
2025-04-22 22:24:27 +0500

    add contact and display

--- contacts/models.py
+++ contacts/models.py
@@ -4,3 +4,17 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Contact(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='contacts')
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('user', 'email')
+    def __str__(self):
+        return self.name
--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required
 def index(request):
-    context = {}
+    contacts = request.user.contacts.all().order_by('-created_at')
+    print("Fetched Contacts:", contacts)  # Debugging statement
+    context = {
+        'contacts': contacts,
+    }
     return render(request, 'contacts.html', context)
